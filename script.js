71b0d212a492c6a97c8ffac31fb0858766c46fa7
MaryKolosova <97162645+MaryKolosova@users.noreply.github.com>
2022-12-18 20:23:43 +0300

    feat: add js index css changes

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+let text = "I'm a Front-End Developer..."
+let i = 0
+let speed = 100
+
+function type() {
+	if (i < text.length) {
+		document.querySelector('#par').textContent += text.charAt(i);
+		i++;
+		setTimeout(type, speed);
+	}
+}
+type()
+
 /* ---- particles.js config ---- */
 
 particlesJS("particles-js", {
